9e9f626175843c9833b812c4ca4d75bcbed35382
Evgeny Vlasov <vlasov.evgeny@gmail.com>
2024-02-06 13:43:21 +0100

    additions and optimization

--- src/Helper/VatHelper.php
+++ src/Helper/VatHelper.php
@@ -14,18 +14,19 @@ class VatHelper
 {
     public static function calculateSumByVat(VatType $type, int|float $sum): float|int
     {
-        $sum = match ($type) {
-            VatType::NONE => 0,
-            VatType::VAT0 => 0,
-            VatType::VAT10 => $sum / (1 + (10 / 100)) * (10 / 100),
-            VatType::VAT18 => $sum / (1 + (18 / 100)) * (18 / 100), // Deprecated 01.04.2019
-            VatType::VAT20 => $sum / (1 + (20 / 100)) * (20 / 100),
-            VatType::VAT110 => $sum / (1 + (10 / 110)) * (10 / 110),
-            VatType::VAT118 => $sum / (1 + (18 / 118)) * (18 / 118), // Deprecated 01.04.2019
-            VatType::VAT120 => $sum / (1 + (20 / 120)) * (20 / 120),
-            default => 0,
+        $vatPercentage = match ($type) {
+            VatType::NONE, VatType::VAT0 => [0, 100],
+            VatType::VAT10 => [10, 100],
+            VatType::VAT18 => [18, 100], // Deprecated 01.04.2019
+            VatType::VAT20 => [20, 100],
+            VatType::VAT110 => [10, 110],
+            VatType::VAT118 => [18, 118], // Deprecated 01.04.2019
+            VatType::VAT120 => [20, 120],
+            default => [0, 100],
         };
 
-        return round($sum, 2);
+        $vat = $sum / ($vatPercentage[0] + $vatPercentage[1]) * $vatPercentage[0];
+
+        return round($vat, 2);
     }
 }
